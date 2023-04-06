0ca07bde6a1fbbe2f34625ee85ee25021b2b51cf
oliver-wahome <olivernderitu135@gmail.com>
2023-04-06 15:18:26 +0300

    completing the Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './App.css'
+import Header from './components/Header';
+import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      App page
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <div className="header">
+            <img src="src/images/troll-face.png" alt="troll-face-icon" height="26px" width="32px" />
+            <p className="headerText">Meme Generator</p>
+        </div>
+    );
+}
+
+export default Header;
